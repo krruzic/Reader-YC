8846d7b9e3bbc7b7adcddc19285dbfc5ab729127
krruzic <krruzic@gmail.com>
2013-08-20 01:44:56 -0600

    fixed display of postTime in search

--- app/HNSearchAPI.py
+++ app/HNSearchAPI.py
@@ -37,22 +37,22 @@ class HackerNewsSearchAPI:
             if second_diff < 120:
                 return  "1 minute ago "
             if second_diff < 3600:
-                return str( second_diff / 60 ) + " minutes ago "
+                return str( second_diff // 60 ) + " minutes ago "
             if second_diff < 7200:
                 return "1 hour ago "
             if second_diff < 86400:
-                return str( second_diff / 3600 ) + " hours ago "
+                return str( second_diff // 3600 ) + " hours ago "
         if day_diff == 1:
             return "Yesterday "
         if day_diff < 7:
             return str(day_diff) + " days ago "
         if day_diff < 31:
-            return str(day_diff//7) + " weeks ago "
+            return str(day_diff // 7) + " weeks ago "
         if day_diff < 365:
-            return str(day_diff//30) + " months ago "
-        if str(day_diff//365) == '1':
-            return str(day_diff//365) + " year ago "
-        return str(day_diff//365) + " years ago "
+            return str(day_diff // 30) + " months ago "
+        if str(day_diff // 365) == '1':
+            return str(day_diff // 365) + " year ago "
+        return str(day_diff // 365) + " years ago "
 
     def getResults(self, startIndex, source):
         """
